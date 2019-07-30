7c2a549d51ca95d45146b2aff7d7c4e510f93b08
Dave Masilungan <MASILUNGANDA@innovasi.com>
2019-07-30 00:47:46 -0700

    added babel/ eslint configs - added file folder structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,18 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {
+  useStaticQuery,
+  graphql
+} from "gatsby"
 
-import Header from "./header"
+import Header from "./header/header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Layout = ({
+  children
+}) => {
+  const data = useStaticQuery(graphql `
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,25 +28,34 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+  return ( <
+    >
+    <
+    Header siteTitle = {
+      data.site.siteMetadata.title
+    }
+    /> <
+    div style = {
+      {
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }
+    } >
+    <
+    main > {
+      children
+    } < /main> <
+    footer > ©{
+      new Date().getFullYear()
+    }, Built with {
+      ` `
+    } <
+    a href = "https://www.gatsbyjs.org" > Gatsby < /a> <
+    /footer> <
+    /div> <
+    />
   )
 }
 
