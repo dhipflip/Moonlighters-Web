dcbc29d279b1726ea35701dc3e92acdcfa0a8193
Dave <davemasilungan@gmail.com>
2019-08-21 21:32:29 -0700

    minor css updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Moonlighters" />
+    <imgs></imgs>
     <h1>Moonlighters</h1>
     <p>This is the new Moonlighters website.</p>
     <Link to="/contact/">Contact Page</Link>
